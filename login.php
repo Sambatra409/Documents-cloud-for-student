b28d5b0c5039bc9a9bfb1839e5980a1c31f4d022
Sambatra409 <rkts409@gmail.com>
2025-11-26 20:20:28 +0300

    first update

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <form action="log.php" method="post">
+        Nom d'utilisateur <br>
+        <input type="text" label ="Entrer votre nom d'utilisateur"> <br>
+        Mot de passe <br>
+        <input type="password"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
